70632a3a262fa8646299f7fba3fb92a80bc34767
Ryan Houck <ryan@levelskies.com>
2017-02-13 06:59:33 -0800

    added feed forward and rnn to models, updaed train function to better handle sequence type training

--- nn/model.py
+++ nn/model.py
@@ -20,17 +20,21 @@ class adagrad_lr(object):
         self.ss_grad = merge_with(sum, self.ss_grad, valmap(np.square, grad))
         return valmap(self.f, self.ss_grad)
 
-class BaseModel(object):
+class LinearModel(object):
     """general model building class
     provide Weights dictionary
     l2 norm regularization not applied to bias units denoted with 'b' in weights
     """
-    def __init__(self, final_layer, lr=1e-1, reg=0,):
-        self.fl = final_layer
+    def __init__(self, in_size, out_size, fl_act, lr=1e-1, reg=0,):
+        
+        self.in_size = in_size
+        self.out_size = out_size
+        self.fl_act = fl_act
         self.lr = lr
         self.reg = reg
-        self.W = {'None': None}
-    
+        self.W = {'xy': self.init_weights(self.in_size, self.out_size),
+                  'b': np.zeros((1, self.out_size,))} 
+   
     def init_weights(self, *args):
         return np.random.randn(*args) * 0.01
         #return 2 * np.random.random(args) - 1
@@ -41,7 +45,7 @@ class BaseModel(object):
         else:
             return kwargs[key]
         
-    def est_grad(self, X, y):               
+    def est_grad(self, X, y):
         def ind_W(W_key):
             W = self.W[W_key]
             ep = 1e-5
@@ -49,10 +53,11 @@ class BaseModel(object):
             for i in np.ndenumerate(W):
                 ys = []
                 for k in (op.add, op.sub):
+                    if hasattr(self, 'reset'): self.reset()
                     W_mod = np.array(W, copy=True)
                     W_mod[i[0]] = k(i[1], ep)
                     pred = self.predict(X, **{W_key: W_mod})
-                    loss = self.fl.loss(y, pred)
+                    loss = self.calc_loss(y, pred)
                     loss += self.calc_reg_loss(**{W_key: W_mod})
                     ys.append(loss)
                 grad = np.subtract(*ys) / (2 * ep) 
@@ -80,11 +85,25 @@ class BaseModel(object):
             else:
                 dW[key] = self.W[key] * self.reg
         return dW
+
+    def predict(self, X, **kwargs):
+        w = partial(self.set_if_given, kwargs)
+        l1 = np.dot(X, w('xy')) + w('b')
+        return self.fl_act.activate(l1)
+
+    def calc_loss(self, y, pred):
+        return self.fl_act.loss(y, pred)
+        
+    def calc_grad(self, X, y, pred):
+        dpred = self.fl_act.dloss(y, pred)
+        dW_xy = np.dot(X.T, dpred)
+        dW_b = np.sum(dpred, axis=0, keepdims=True)
+        return {'xy': dW_xy, 'b': dW_b}
     
     def step(self, X, y):
         pred = self.predict(X)
         
-        loss = self.fl.loss(y, pred)
+        loss = self.calc_loss(y, pred)
         loss += self.calc_reg_loss()
         
         grad = self.calc_grad(X, y, pred)
@@ -99,4 +118,98 @@ class BaseModel(object):
         for key in grad.keys():
             self.W[key] += grad[key] * -lr[key]
 
+        return loss
+
+class FeedFwd(LinearModel):
+    
+    def __init__(self, in_size, hidden_size, out_size, hl_act, fl_act, **kwargs):
+        super(FeedFwd, self).__init__(in_size, out_size, fl_act, **kwargs)
+        self.hidden_size = hidden_size
+        self.hl_act = hl_act
+        self.W = {'xl1': self.init_weights(self.in_size, self.hidden_size),
+                  'bl1': np.zeros((1, self.hidden_size,)),
+                  'l1y': self.init_weights(self.hidden_size, self.out_size),
+                  'by': np.zeros((1, self.out_size,)),}
+    
+    def predict(self, X, **kwargs):
+        w = partial(self.set_if_given, kwargs)
+        l1 = self.hl_act(np.dot(X, w('xl1')) + w('bl1'))
+        l2 = np.dot(l1, w('l1y')) + w('by')
+        self.layers = [l1, l2]
+        return self.fl_act.activate(l2)
+        
+    def calc_grad(self, X, y, pred):
+        dpred = self.fl_act.dloss(y, pred)
+        
+        dW_l1y = np.dot(self.layers[0].T, dpred)
+        dW_by = np.sum(dpred, axis=0, keepdims=True)
+        
+        d_l1 = np.dot(dpred, self.W['l1y'].T)
+        d_l1 = d_l1 * self.hl_act(self.layers[0], deriv=True)
+        
+        dW_xl1 = np.dot(X.T, d_l1)
+        dW_b1 = np.sum(d_l1, axis=0, keepdims=True)
+        
+        return {'xl1': dW_xl1, 'bl1': dW_b1, 'l1y': dW_l1y, 'by': dW_by}
+
+class RNN(FeedFwd):
+    
+    def __init__(self, *args, **kwargs):
+        self.batch_size = kwargs.pop('batch_size', 1)
+        super(RNN, self).__init__(*args, **kwargs)
+        self.reset()
+        self.W = {'xh': self.init_weights(self.in_size, self.hidden_size),
+                  'hh': self.init_weights(self.hidden_size, self.hidden_size), 
+                  'bh': np.zeros((1, self.hidden_size,)),    
+                  'hy': self.init_weights(self.hidden_size, self.out_size),
+                  'by': np.zeros((1, self.out_size,)),}
+    
+    def reset(self):
+        self.hs = [np.zeros([self.batch_size, self.hidden_size])]
+        self.dh = np.zeros([self.batch_size, self.hidden_size])
+    
+    def _predict(self, X, **kwargs):    
+        w = partial(self.set_if_given, kwargs)        
+        terms = [np.dot(self.hs[-1], w('hh')),
+                 np.dot(X, w('xh')),
+                 w('bh')]
+        h = self.hl_act(reduce(op.add, terms))
+        l2 = np.dot(h, w('hy')) + w('by')
+        
+        self.hs.append(h)         
+        return self.fl_act.activate(l2)
+    
+    def predict(self, Xs, **kwargs):
+        return map(lambda x: self._predict(x, **kwargs), Xs)
+    
+    def calc_loss(self, ys, preds):
+        losses = map(lambda x: self.fl_act.loss(*x), zip(ys, preds))
+        return sum(losses) / len(losses)
+    
+    def _calc_grad(self, X, y, pred, i):
+        dpred = self.fl_act.dloss(y, pred)
+        
+        dw_hy = np.dot(self.hs[i].T, dpred)
+        dw_yb = np.sum(dpred, axis=0, keepdims=True)
+        
+        d_l1 = np.dot(dpred, self.W['hy'].T) + self.dh
+        d_l1 = d_l1 * self.hl_act(self.hs[i], deriv=True)
+        
+        dw_xh = np.dot(X.T, d_l1)
+        dw_hh = np.dot(self.hs[i-1].T, d_l1)
+        dw_hb = np.sum(d_l1, axis=0, keepdims=True)
+        
+        self.dh = np.dot(d_l1, self.W['hh'].T)
+        
+        return {'xh': dw_xh, 'hh': dw_hh, 'bh': dw_hb, 'hy': dw_hy, 'by': dw_yb}
+    
+    def calc_grad(self, Xs, ys, preds):
+        inds = reversed(range(len(Xs)))
+        grads = map(lambda ind: self._calc_grad(Xs[ind], ys[ind], preds[ind], ind+1), inds)
+        grads_sum = reduce(partial(merge_with, sum), grads)
+        return valmap(lambda x: x / len(Xs), grads_sum)
+        
+    def step(self, *args, **kwargs):
+        loss = super(RNN, self).step(*args, **kwargs) 
+        self.reset()
         return loss
--- nn/utils.py
+++ nn/utils.py
@@ -1,101 +1,60 @@
 import json
+from operator import add
 
 import numpy as np
+from toolz import first, second, accumulate
 from toolz.dicttoolz import merge_with, valmap
 
-def accuracy(mod, X, y):
+
+def accuracy(y, pred):
+    if all(map(lambda x: isinstance(x, list), (y, pred))):
+        accs = map(lambda x: accuracy(*x), zip(y, pred))
+        return sum(accs) / float(len(accs))
     f = np.vectorize(lambda x: True if x == 0 else False)
-    correct = f(np.argmax(mod.predict(X), axis=1) - y)
+    correct = f(np.argmax(pred, axis=1) - y)
     return sum(correct) / float(len(correct))
 
-def check_grads(grad, est_grad):
-    
-    f = np.vectorize(lambda x: 1 if x < 1e-2 else 0)
-    
+def check_grads(grad, est_grad):  
+    f = np.vectorize(lambda x: 1 if x < 1e-2 else 0)   
     checks = {}
     for key in grad.keys():
         abs_diff = abs(grad[key] - est_grad[key])
         sum_grads = abs(grad[key]) + abs(est_grad[key]) 
         rel_error = abs_diff / np.maximum(1e-10, sum_grads)
-        checks[key] =  round(f(rel_error).sum() / float(rel_error.size), 2)
-        #checks[key] = np.mean(rel_error) < 1e-2
-
-        # non_zero_grad = np.maximum(1e-10, abs(grad[key]))
-        # mean_diff_rat = np.mean(abs_diff / non_zero_grad)
-        # checks[key] = mean_diff_rat < 1e-1
+        checks[key] =  round(f(rel_error).sum() / float(rel_error.size), 2) 
     return checks
 
-# class MiniBatch(object):
-#     def __init__(self, X, y, batch_size):
-#         self.X = X
-#         self.y = y
-#         self.batch_size = batch_size
-
-#     def num_batch_per_epoch():
-#         return int(len(self.X) / self.batch_size)
-
-#     def gen(self):
-#         while True:
-#             inds = np.random.choice(np.arange(len(self.X)), size=n, replace=False)
-#             yield self.X[inds], self.y[inds]
-
-# class SeqMiniBatch(MiniBatch):
-#     def __init__(self, *args, **kwargs):
-#         super(SeqMiniBatch, self).__init__(*args, **kwargs)
-#         self.sequence_len = sequence_len
-
-#     def gen(self):
-#         count = 0
-#         max_ind = len(self.X) - self.sequence_len - 1
-#         while True:  
-#             if not count:
-#                 inds = np.random.choice(np.arange(max_ind), size=self.batch_size, replace=False)
-#             else:
-#                 inds += 1
-            
-#             count += 1
-#             if count == self.sequence_len:
-#                 count = 0
-            
-#             yield self.X[inds], self.y[inds]
-
-
-def get_minibatch(X, y, n=None):
-    if not n:
-        n = len(X)
+def get_minibatch(X, y, batch_size=None):
+    batch_size = len(X) if not batch_size else batch_size
     while True:
-        inds = np.random.choice(np.arange(len(X)), size=n, replace=False)
-        #np.random.shuffle(inds)
+        inds = np.random.choice(np.arange(len(X)), size=batch_size, replace=False)
         yield X[inds], y[inds]
 
 def get_seq_minibatch(X, y, batch_size, sequence_len):    
-    count = 0
     max_ind = len(X) - sequence_len - 1
-    while True:  
-        if not count:
-            inds = np.random.choice(np.arange(max_ind), size=batch_size, replace=False)
-        else:
-            inds += 1
-        
-        count += 1
-        if count == sequence_len:
-            count = 0
-        
-        yield X[inds], y[inds]
+    while True:
+        inds = np.random.choice(np.arange(max_ind), size=batch_size, replace=False)
+        inds_inc = accumulate(add, [inds] + [1] * (sequence_len - 1))        
+        yield map(lambda inds: (X[inds], y[inds]), inds_inc)
 
+def is_sequence_type(Xy):
+    a = not isinstance(Xy[0], np.ndarray)
+    b = isinstance(Xy[0][0], np.ndarray)
+    return all([a, b])
 
-def train(mod, data_gen, num_batch_per_epoch, nepochs=100, check=True, sequence_len=None):
+def split_Xy(Xy):
+    if not is_sequence_type(Xy):
+        X, y = Xy
+    else:
+        X = map(first, Xy)
+        y = map(second, Xy)
+    return X, y
+
+def train(mod, data_gen, num_batch_per_epoch, nepochs=100, check=True):
     
-    def grad_accuracy(mod, X, y):
-        pred = mod.predict(X)
-        grad = mod.calc_grad(X, y, pred)
-        grad = merge_with(sum, grad, mod.calc_dreg_loss())
-        est_grad = mod.est_grad(X, y)
-        return check_grads(grad, est_grad) 
-
-    def get_acc(mod, X, y):
+    def get_acc(y, pred):
         try:
-            return round(accuracy(mod, X, y), 2)
+            return round(accuracy(y, pred), 2)
         except:
             return np.nan
 
@@ -103,41 +62,33 @@ def train(mod, data_gen, num_batch_per_epoch, nepochs=100, check=True, sequence_
     try:
         for epoch in range(nepochs):
             for batch in range(num_batch_per_epoch):
-        
-                if not sequence_len:
-                    X, y = data_gen.next()
-                    loss = mod.step(X, y)
-                else:
-                    for step in range(sequence_len):
-                        X, y = data_gen.next()
-                        _ = mod.step(X, y)
+                Xy = data_gen.next()
+                X, y = split_Xy(Xy)
+                _ = mod.step(X, y)
             
-            # calc performance
-            if epoch and not epoch % int((nepochs * .1)):
+            # check performance
+            if epoch and not epoch % int((nepochs * .05)):
+                                
+                Xy = data_gen.next()
+                X, y = split_Xy(Xy)
+                pred = mod.predict(X)
+                loss = mod.calc_loss(y, pred)
+                loss += mod.calc_reg_loss()
+                acc = get_acc(y, pred)
                 
-                if not sequence_len:
-                    acc = get_acc(mod, X, y)
-                    if check and epoch <= (nepochs * .2):                
-                        print grad_accuracy(mod, X, y)
-                else:
-                    loss, acc =  0, 0
-                    for step in range(sequence_len):
-                        X, y = data_gen.next()
-                        if check and step == 1 and epoch <= (nepochs * .2):                
-                           print grad_accuracy(mod, X, y)      
-
-                        loss += mod.step(X, y)
-                        acc += get_acc(mod, X, y)
-
-                    loss /= float(sequence_len)
-                    acc /= float(sequence_len)
-
-                stats.append((epoch, loss, acc))
+                stats.append((epoch, loss, acc, ))
                 print('epoch {0}\tloss: {1:0.5f}\tacc: {2}'.format(*stats[-1]))
+  
                 
+                if check and epoch <= (nepochs * .2):
+                    grad = mod.calc_grad(X, y, pred)
+                    grad = merge_with(sum, grad, mod.calc_dreg_loss())
+                    est_grad = mod.est_grad(X, y)
+                    print check_grads(grad, est_grad)
+                  
     except KeyboardInterrupt as err:
         print('stopping optimzation')
-
+    
     return stats
 
 def write_weights(fn, W):
