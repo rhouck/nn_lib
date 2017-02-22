785a24b6aa588ae6334b7bd8a42700a5729ab0b2
Ryan Houck <ryan@levelskies.com>
2017-02-22 09:36:06 -0800

    added nlp tools for keras models

--- nn/nlp.py
+++ nn/nlp.py
@@ -1,5 +1,9 @@
+from functools import partial
+
 import numpy as np
 from sklearn import preprocessing
+from toolz.dicttoolz import itemmap
+from toolz import compose
 
 
 class Encoder(object):
@@ -22,7 +26,36 @@ class Encoder(object):
         ind = self.to_ind(char)
         return self.enc.transform(ind).toarray()[0]
 
-def gen_sample(mod, enc, seed_val, sequence_len):
+class SpacyEncoder(object):
+    def __init__(self, nlp, text):
+        self.nlp = nlp
+        text = self.nlp(unicode(text))
+    
+        summary = 'total word count:\t{0}'.format(len(text))
+
+        mapped_count = len([i for i in text if i.has_vector])
+        summary += '\nmapped word count:\t{0}'.format(mapped_count)
+                
+        unique_words = {i.text for i in text}
+        self.word_count = len(unique_words)
+        summary += '\nnum unique words:\t{0}'.format(self.word_count)
+        print(summary)
+
+        self.lookup = {i: ind for ind, i in enumerate(unique_words)}
+        self.inds = itemmap(reversed, self.lookup)
+        self.text = text
+        
+    def to_vect(self, word):
+        ind = self.nlp.vocab.strings[word]
+        return self.nlp.vocab[ind].vector
+
+    def ind_to_target_vect(self, ind):
+        v = np.zeros(self.word_count)
+        v[ind] = 1.
+        return v
+       
+
+def gen_sample(mod, enc, seed_val, sequence_len, sep=''):
     mod.reset()
     ys = [seed_val,]
     for i in range(sequence_len):
@@ -31,4 +64,63 @@ def gen_sample(mod, enc, seed_val, sequence_len):
         ind = np.random.choice(range(len(pred)), p=pred.ravel())
         ys.append(enc.inds[ind])
 
-    return ''.join(ys)
+    return sep.join(ys)
+
+
+MISSINGWORD = 'MISSINGWORD'
+
+def drop_unkown_and_rare_words(nlp, text):
+    counts = {}
+    doc = nlp.make_doc(unicode(text))
+    for i in doc:
+        ind = nlp.vocab.strings[i.text]
+        try:
+            counts[ind] += 1
+        except:
+            counts[ind] = 1
+    
+    text_clean = ''
+    for i in doc:    
+        ind = nlp.vocab.strings[i.text] 
+        too_infrequent = counts[ind] < 2
+        conds = (too_infrequent, i.is_oov)
+        text_clean += MISSINGWORD if any(conds) else i.text
+        text_clean += str(i.whitespace_)
+        
+    return text_clean
+
+def encode_inp(enc, text):
+    inp = enc.nlp(unicode(text))
+    inp = [i.vector for i in inp]
+    return np.array([inp])
+
+def fill_sequence(sequence_len, Xs):
+    Xs = Xs[:,-sequence_len:,:] # truncate too long of inputs
+    sequence_missing = sequence_len - Xs.shape[1]
+    if sequence_missing:
+        shape = list(Xs.shape)
+        shape[1] = sequence_missing
+        padding = np.zeros(shape)
+        Xs = np.concatenate([padding, Xs], axis=1)
+    return Xs
+
+def sample_next(enc, mod, Xs):
+    pred = mod.predict(Xs)[0]
+    for i in range(5):
+        ind = np.random.choice(range(len(pred)), p=pred.ravel())
+        word = enc.inds[ind]
+        if word != MISSINGWORD:
+            break
+    vect = enc.to_vect(word)
+    Xs_ext = np.concatenate([Xs, np.array([[vect]])], axis=1)
+    return word, vect, Xs_ext
+    
+def gen_string(enc, mod, sequence_len, text, n):
+    Xs = encode_inp(enc, text.lower())
+    fs = compose(partial(sample_next, enc, mod), 
+                 partial(fill_sequence, sequence_len))
+    ys = []
+    for i in range(n):
+        y, vect, Xs = fs(Xs)
+        ys.append(y)
+    return text + ' ' + ' '.join(ys)
